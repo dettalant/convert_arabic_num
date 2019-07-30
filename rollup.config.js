8b3b9d14ebf439f3cce1f0788e5ab4c742a9e5f9
dettalant <lilac@dettalant.com>
2019-07-31 01:07:10 +0900

    turn to umd module

--- rollup.config.js
+++ rollup.config.js
@@ -40,7 +40,7 @@ export default {
   input: "./src/index.ts",
   output: {
     file: fileName + ".js",
-    format: "esm",
+    format: "umd",
     name: scriptArgs.name,
     banner: bannerComment,
     sourceMap: "inline",
